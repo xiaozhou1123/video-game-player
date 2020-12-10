0473783b2e993402edbf2c525e95b90c47ef440a
xiaozhou1123 <54874804+xiaozhou1123@users.noreply.github.com>
2020-12-10 21:32:21 +0800

    important

---
+++ sys.h
@@ -0,0 +1,16 @@
+#ifndef __SYS_H
+#define __SYS_H	
+
+#include <reg51.h>
+#include <intrins.h>
+
+#define	u8 unsigned char
+#define	u16 unsigned int
+#define	u32 unsigned long
+   		 
+#endif  
+	 
+	 
+
+
+
